1af62431570df3b73e02417efc6ece0a4c8c98dc
runarut <runarut@users.noreply.github.com>
2023-10-28 10:03:19 -0500

    bug fix and apps do not increment to boot count.

--- bin/wthr_logger.sh
+++ bin/wthr_logger.sh
@@ -9,7 +9,7 @@ export RESTART_DELAY=10
 export APP_HOME="/home/$(whoami)/telemetry-data"
 export APP_TMP_DIR="${APP_HOME}/tmp"
 export APP_BASE_PATH="${APP_HOME}/data"
-export APP_PYTHON=python3.10
+export APP_PYTHON="/home/$(whoami)/.local/bin/python3.11"
 export DEBUG="True"
 export SHARED_DICTIONARY_NAME="TELEMETRY"
 
@@ -17,7 +17,7 @@ export SHARED_DICTIONARY_NAME="TELEMETRY"
 export APP_COUNT=$(${APP_PYTHON} -m counter.app_counter ${APP_ID})
 
 # get current system startup counter
-export BOOT_COUNT=$(${APP_PYTHON} -m counter.boot_counter)
+export BOOT_COUNT=$(${APP_PYTHON} -m counter.boot_counter --current_boot_count)
 
 export APP_LOG_FILE="telemetry-${BOOT_COUNT}-${APP_ID}-${APP_COUNT}.log"
 
