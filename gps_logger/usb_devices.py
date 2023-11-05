c1bd0136c5bc3cb533c2b564062f999c7b38f789
runarut <runarut@users.noreply.github.com>
2023-11-05 09:44:27 -0600

    gps_logger.usb_devices - main now exits with 1 when device not found

--- gps_logger/usb_devices.py
+++ gps_logger/usb_devices.py
@@ -1,5 +1,7 @@
 # telemetry-gps/gps_logger/usb_devices.py
 
+import logging
+from sys import stderr
 from serial.tools.list_ports import comports
 
 # The following are specific to the u-blox GNSS receiver
@@ -10,7 +12,7 @@ GPS_DEVICE_NAME = "u-blox GNSS receiver"
 # This default is the default device name on a Raspberry Pi when the GPS is the only attached serial USB device.
 DEFAULT_SERIAL_DEVICE="/dev/ttyACM0"
 
-def get_serial_device_name(verbose=False)->str:
+def get_serial_device_name(verbose=False, default_serial_device=DEFAULT_SERIAL_DEVICE)->str:
     """Get serial device name for GPS"""
 
     for p in comports():
@@ -18,36 +20,39 @@ def get_serial_device_name(verbose=False)->str:
             return p.device
 
     logging.error(f"USB attached GPS device <{GPS_DEVICE_NAME}> not found.")
-    logging.error(f"Trying serial port device <{DEFAULT_SERIAL_DEVICE}>.")
+    logging.info(f"Trying serial port device <{default_serial_device}>.")
 
-    return DEFAULT_SERIAL_DEVICE
+    return default_serial_device
 
 def main():
-    print("Candidate Serial Device List (non-USB devices excluded)")
+    logging.basicConfig(stream=stderr, level=logging.DEBUG)
+
+    logging.info("Candidate Serial Device List (non-USB devices excluded)")
     i = 0
     for p in comports():
         if not p.vid:
             # not a USB device
             continue
         i += 1
-        print(f"\n\t+{i} {p.device}")
-        print(f"\t\tName: {p.name}")
-        print(f"\t\tUSB VID: {p.vid}")
-        print(f"\t\tUSB PID: {p.pid}")
-        print(f"\t\tDescription: {p.description}")
-        print(f"\t\tHardware ID: {p.hwid}")
-        print(f"\t\tManufacturer: {p.manufacturer}")
-        print(f"\t\tProduct: {p.product}")
-        print(f"\t\tSerial Number: {p.serial_number}")
-        print(f"\t\tLocation: {p.location}")
-        print(f"\t\tinterface: {p.interface}")
+        logging.info(f"\n\t+{i} {p.device}")
+        logging.info(f"\t\tName: {p.name}")
+        logging.info(f"\t\tUSB VID: {p.vid}")
+        logging.info(f"\t\tUSB PID: {p.pid}")
+        logging.info(f"\t\tDescription: {p.description}")
+        logging.info(f"\t\tHardware ID: {p.hwid}")
+        logging.info(f"\t\tManufacturer: {p.manufacturer}")
+        logging.info(f"\t\tProduct: {p.product}")
+        logging.info(f"\t\tSerial Number: {p.serial_number}")
+        logging.info(f"\t\tLocation: {p.location}")
+        logging.info(f"\t\tinterface: {p.interface}")
 
-    print(f"\nFound {i} USB Serial Device(s)")
+    logging.info(f"\nFound {i} USB Serial Device(s)")
 
-    if sdn := get_serial_device_name():
-        print(f"\nUSB Serial Device <{GPS_DEVICE_NAME}> Name {sdn} found")
+    if sdn := get_serial_device_name(default_serial_device=None):
+        logging.info(f"\nUSB Serial Device <{GPS_DEVICE_NAME}> Name {sdn} found")
+        exit(0)
     else:
-        print("\nUSB Serial Device NOT found")
+        exit(1)
 
 if __name__ == "__main__":
     main()
