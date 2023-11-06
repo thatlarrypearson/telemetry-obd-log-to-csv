753cee405a85fe0d08b302bf1b03ae2c2d6df7ec
runarut <runarut@users.noreply.github.com>
2023-11-06 10:12:27 -0600

    add build/install script

---
+++ bin/Install_gps.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+# Install_gps.sh
+#
+
+export APP_HOME="/home/$(whoami)/telemetry-gps"
+export APP_PYTHON="/home/$(whoami)/.local/bin/python3.11"
+export DEBUG="True"
+
+# Debugging support
+if [ "${DEBUG}" = "True" ]
+then
+	# enable shell debug mode
+	set -x
+fi
+
+cd ${APP_HOME}
+
+if [ -d "${APP_HOME}/dist" ]
+then
+	rm -rf "${APP_HOME}/dist"
+fi
+
+${APP_PYTHON} -m build .
+ls -l dist/*.whl
+${APP_PYTHON} -m pip install --force-reinstall dist/*.whl
+
+${APP_PYTHON} -m gps_logger.gps_logger --help
+${APP_PYTHON} -m gps_logger.usb_devices --help
